358cdba602c531ce5a56b15d5dade9ae3da906d9
Brian <baustin27@gmail.com>
2018-05-05 20:47:43 -0700

    styling update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var planets = [
+const planets = [
     ['Pluto', 0.06],
     ['Neptune', 1.148],
     ['Uranus', 0.917],
@@ -14,11 +14,11 @@ var planets = [
 //below reverses the order of the array, therefore reversing the order the planets appear in the UI dropdown list. A bonus item for the project.
 planets.reverse();
 
-var sel = document.getElementById('planets');
-var fragment = document.createDocumentFragment();
+const sel = document.getElementById('planets');
+const fragment = document.createDocumentFragment();
 
 planets.forEach(function(planet, index) {
-    var opt = document.createElement('option');
+    let opt = document.createElement('option');
     opt.innerHTML = planet[0];
     opt.value = planet[0];
     fragment.appendChild(opt);
@@ -29,20 +29,19 @@ sel.appendChild(fragment);
 function calculateWeight(weight, planetName) {
     weight = document.getElementById('user-weight').value;
     planetName = document.getElementById('planets').value;
-    var newWeight = 0;
-    for (var i = 0; i < planets.length; i++) {
+    let newWeight = 0;
+    for (let i = 0; i < planets.length; i++) {
         if (planets[i][0] === planetName) {
             newWeight = planets[i][1] * weight;
-            break;
         }
     }
     return newWeight;
 };
 
 function handleClickEvent(e) {
-    var userWeight = document.getElementById('user-weight').value;
-    var planetName = document.getElementById('planets').value;
-    var result = calculateWeight();
+    let userWeight = document.getElementById('user-weight').value;
+    let planetName = document.getElementById('planets').value;
+    let result = calculateWeight();
     document.getElementById('output').innerHTML = 'If you were on ' + planetName + ', you would weigh ' + result + 'lbs!';
 };
 
